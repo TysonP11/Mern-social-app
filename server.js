adecb5f49bdcb803bf299652c7a70bb11e77e22e
TysonP11 <59603295+TysonP11@users.noreply.github.com>
2020-04-01 00:55:30 +0700

    Initial commit

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = 5000 || process.env.port;
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
-});
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('API running');
+});
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
 });
