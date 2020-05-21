3b6e7dc38b2ab8125ff96451e3ab497020c5c905
TysonP11 <59603295+TysonP11@users.noreply.github.com>
2020-05-22 00:09:35 +0700

    added facebookTokenStrategy (works with postman)

--- routes/api/oAuth/facebook/auth.js
+++ routes/api/oAuth/facebook/auth.js
@@ -7,14 +7,34 @@ const config = require('config');
 const passport = require('passport');
 const auth = require('../../../../middleware/auth');
 
-router.get(
-  '/',
-  passport.authenticate('facebook', {
-    scope: ['email'],
-  })
-);
+// router.get(
+//   '/',
+//   passport.authorize('facebook', {
+//     failureRedirect: '/register',
+//     scope: ['email'],
+//   })
+// );
 
-router.get('/callback', passport.authenticate('facebook'), async (req, res) => {
+// router.get('/callback', passport.authorize('facebook'), async (req, res) => {
+//   const payload = {
+//     user: {
+//       id: req.user.id,
+//     },
+//   };
+
+//   jwt.sign(
+//     payload,
+//     config.get('jwtSecret'),
+//     { expiresIn: 360000 },
+//     (err, token) => {
+//       if (err) throw err;
+//       res.json({ token });
+//     }
+//   );
+// });
+
+router.get('/', passport.authenticate('facebookToken'), async (req, res) => {
+  console.log('got here.');
   const payload = {
     user: {
       id: req.user.id,
