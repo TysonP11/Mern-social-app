86335a2cd4d314993f28a53e208d88e26b7205b4
TysonP11 <59603295+TysonP11@users.noreply.github.com>
2020-05-25 22:13:39 +0700

    added config

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '332181767747129',
+    clientSecret: '321452ac7aa8c6e06e72718085c228f9',
+    callbackURL: '/api/oAuth/facebook/callback',
+  },
+  google: {
+    clientID:
+      '113765765033-i4gafkqoaqmpl5qke12nrbqhh0jknd95.apps.googleusercontent.com',
+    clientSecret: 'ujfQ-U-FcXFDUjHuPBrdqGgH',
+  },
+  session: {
+    cookieKey: 'mycookiekeyisasecretlmao',
+  },
+};
